6594df4e7cd04b5d8f6b2f5957d22b2b16ce45e1
MyWasowsky <gemetrov@gmaim.com>
2025-09-22 19:42:29 -0300

    dep

---
+++ src/MiCompPolitica.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MiCompCultura() {
+    return (
+        <div>
+            <p>🇦🇷 El Teatro Colón de Buenos Aires estrenó una nueva temporada de ópera con una puesta renovada de "La Traviata".</p>
+        </div>
+    );
+}
+
+export default MiCompCultura;
